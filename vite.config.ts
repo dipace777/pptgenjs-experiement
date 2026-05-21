a6efe9435a436007a27b368d090b5c8ca0b73fd4
Dipesh Chaulagain <codeict.dipesh@gmail.com>
2026-05-22 04:55:28 +0545

    feat: support nodejs deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tanstackStart(), react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [tanstackStart(), nitro(), react()],
 });
